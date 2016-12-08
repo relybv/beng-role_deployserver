8ae0cc79458fb370d50299d8dcb34549eff0cea6
P. Gomersbach <p.gomersbach@rely.nl>
2016-12-08 15:52:29 +0000

    fix lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,9 +12,9 @@ class role_deployserver
   # a role includes one or more profiles and at least a 'base' profile
 
   class { '::profile_base':
-    tcp_extra_rule1           => true,
-    tcp_extra_rule1_dport     => '8140',
-    tcp_extra_rule1_source    => '0.0.0.0/0',
+    tcp_extra_rule1        => true,
+    tcp_extra_rule1_dport  => 8140,
+    tcp_extra_rule1_source => '0.0.0.0/0',
   }
   class { '::profile_puppetmaster':
     require => Class['::profile_base'],
